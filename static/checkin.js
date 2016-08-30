4856a4989792190470bd175847f5a1805110d918
Falco Nogatz <fnogatz@gmail.com>
2016-08-30 22:05:14 +0200

    Move to OSM tile server

--- static/checkin.js
+++ static/checkin.js
@@ -4,7 +4,7 @@ var map;
 
 document.addEventListener('DOMContentLoaded', function() {
   map = L.map('map').setView(centerOfUlm, 13);
-  L.tileLayer('http://{s}.tile.cloudmade.com/bcaf462f30bd4c02a7378b1bc17dd6b6/997/256/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
   }).addTo(map);
 
